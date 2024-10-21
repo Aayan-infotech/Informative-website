a07a111b32f63927c7523fd59ae56f96438bcbfd
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-10-21 21:19:41 +0530

    add change

--- script.js
+++ script.js
@@ -820,7 +820,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <line x1="12" y1="8" x2="12" y2="12"></line>
         <line x1="12" y1="16" x2="12.01" y2="16"></line>
       </svg>
-     <p style="font-size: 35px; color: #777;">Sorry, No Category Found!</p>
+     <p style="font-size: 35px; color: #777;">No sub-category are currently listed under this category!</p>
   </div>
   `;
   }
